8613114be559636bee84c26ed7d03232d57cf793
calmpsycho333 <wrathking75@yahoo.com>
2020-11-19 17:15:48 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,8 @@
-class person
+class Person
     def talk
         puts "Hello World!"
+    end
         def walk
-            puts "The person is walking"
-end
+            puts "The Person is walking"
 end
 end
